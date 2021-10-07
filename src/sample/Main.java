7f1292b9a9f4cf6d14a2aed53020d2b02ea30569
Ben-Fouch <fouchb@msoe.edu>
2021-10-07 11:17:10 -0500

    fix gitignore

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,11 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        URL resource = getClass().getResource("sample.fxml");
+        if (re)
+        //noinspection ConstantConditions
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
