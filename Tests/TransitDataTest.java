82e607613c8c8ab04346722ac3b1a809a5040a7e
Ben-Fouch <fouchb@msoe.edu>
2021-10-11 10:17:40 -0500

    First Test

---
+++ Tests/TransitDataTest.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransitDataTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testFinalize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void exportFiles() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAverageSpeed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumTrips() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTripDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plotBus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void plotMap() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchRoute() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchRouteForTrips() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchStop() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchStopTrip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateAttributes() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void uploadFiles() {
+    }
+}
