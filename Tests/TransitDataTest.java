7656b480d7566d4a7caeefea2e24349c5573466f
zuberih <zuberih@msoe.edu>
2021-10-14 10:46:46 -0500

    tests

--- Tests/TransitDataTest.java
+++ Tests/TransitDataTest.java
@@ -14,6 +14,46 @@ class TransitDataTest {
     void setUp() {
     }
 
+    // Tests isStops method with to check different headers
+    // to see if it returns the expected results.
+    // Test Case 1: Correct header should return true
+    // Test Case 2: Completely incorrect header should return false
+    // Test Case 3: Partially correct header should return false
+    @Test
+    void isStops() {
+
+    }
+
+    // Tests isTrips method with to check different headers
+    // to see if it returns the expected results.
+    // Test Case 1: Correct header should return true
+    // Test Case 2: Completely incorrect header should return false
+    // Test Case 3: Partially correct header should return false
+    @Test
+    void isTrips() {
+
+    }
+
+    // Tests isStopTimes method with to check different headers
+    // to see if it returns the expected results.
+    // Test Case 1: Correct header should return true
+    // Test Case 2: Completely incorrect header should return false
+    // Test Case 3: Partially correct header should return false
+    @Test
+    void isStopTimes() {
+
+    }
+
+    // Tests isRoutes method with to check different headers
+    // to see if it returns the expected results.
+    // Test Case 1: Correct header should return true
+    // Test Case 2: Completely incorrect header should return false
+    // Test Case 3: Partially correct header should return false
+    @Test
+    void isRoutes() {
+
+    }
+
     @Test
     void testFinalize() {
     }
